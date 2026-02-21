dffbc0aae5ee6bb3a4354393b2cf0646db5548c9
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-21 13:12:41 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,8 +1,6 @@
 const CONFIG = {
-  NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
+  NEXT_HOME_BANNER: false // 首页是否显示大图及标语 [true,false]
   NEXT_HOME_BANNER_STRINGS: [
-    'Hi，我是一个程序员',
-    'Hi，我是一个打工人',
     'Hi，我是一个干饭人',
     '欢迎来到我的博客🎉'
   ], // 首页大图标语文字
@@ -14,12 +12,12 @@ const CONFIG = {
   NEXT_POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
   NEXT_PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
-  NEXT_PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
+  NEXT_PREVIEW_CATEGORY_COUNT: 4, // 首页最多展示的分类数量，0为不限制
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
   NEXT_RIGHT_BAR: true, // 是否显示右侧栏
-  NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
+  NEXT_RIGHT_LATEST_POSTS: false, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
   NEXT_RIGHT_AD: false, // 右侧广告
@@ -32,11 +30,11 @@ const CONFIG = {
   NEXT_MENU_SEARCH: true, // 显示搜索
 
   NEXT_WIDGET_TO_TOP: true, // 是否显示回顶
-  NEXT_WIDGET_TO_BOTTOM: false, // 显示回底
-  NEXT_WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
+  NEXT_WIDGET_TO_BOTTOM: true, // 显示回底
+  NEXT_WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
-  NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
+  NEXT_ARTICLE_RELATE_POSTS: false, // 相关文章推荐
   NEXT_ARTICLE_COPYRIGHT: true, // 文章版权声明
   NEXT_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   NEXT_ARTICLE_INFO: true // 显示文章信息
