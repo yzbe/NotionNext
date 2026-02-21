90b32126a2b042fbcb2985d790f3fa3a50555627
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-21 13:30:41 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
-  NEXT_HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
+  NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
   NEXT_HOME_BANNER_STRINGS: [
+    'Hi，我是一个程序员',
+    'Hi，我是一个打工人',
     'Hi，我是一个干饭人',
     '欢迎来到我的博客🎉'
   ], // 首页大图标语文字
