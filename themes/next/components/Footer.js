a7a3cf039795186a7448c33c131759e58fbb2f87
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-22 18:54:57 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm py-3 bg-white dark:text-gray-400'>
+    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm py-2 bg-white dark:text-gray-400'>
       {/* 使用 flex 布局让所有元素在一行水平居中，gap-x-4 控制元素间的间距 */}
       <div className='flex flex-wrap justify-center items-center gap-x-6'>
     
