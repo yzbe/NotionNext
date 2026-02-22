ef017a2468f76be7f851a6fe6f20e15b406ec739
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-22 18:06:28 +0800

    Update ArticleDetail.js

--- themes/next/components/ArticleDetail.js
+++ themes/next/components/ArticleDetail.js
@@ -78,11 +78,15 @@ export default function ArticleDetail(props) {
                         {post?.publishDay}
                       </div>
                     </SmartLink>
+
+                     {/* --- 更新时间 --- */}
+                      {/*
                     <span className='mr-2'>
                       {' '}
                       | <i className='far fa-calendar-check mr-2' />
                       {post.lastEditedDay}{' '}
                     </span>
+                      */}
 
                     <div className='hidden busuanzi_container_page_pv font-light mr-2'>
                       <i className='mr-1 fas fa-eye' />
