297603ce340f2e42ecd7b52e10fde7d46de43cd7
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-22 18:28:33 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -1,5 +1,4 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
-import DarkModeButton from '@/components/DarkModeButton'
 import { siteConfig } from '@/lib/config'
 
 const Footer = ({ title }) => {
@@ -10,58 +9,52 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    // 【修改点 1】将 p-6 改为 py-2，大幅缩小上下内边距，让底部更窄、更靠下
-    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm py-2 bg-white dark:text-gray-400'>
-      <span>
-        {/* 【修改点 2】注释掉暗黑模式按钮、版权、心形和作者名这一行 */}
-        {/* <DarkModeButton />
-        <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
-        <span className='mx-1 animate-pulse'>
-          <i className='fas fa-heart' />
-        </span>{' '}
-        <a href={siteConfig('LINK')} className='underline font-bold '>
-          {siteConfig('AUTHOR')}
-        </a>
-        .<br /> */}
+    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm py-4 bg-white dark:text-gray-400'>
+      {/* 使用 flex 布局让所有元素在一行水平居中，gap-x-4 控制元素间的间距 */}
+      <div className='flex flex-wrap justify-center items-center gap-x-4'>
+        
+        {/* 1. 版权图标与年份 */}
+        <span>
+          <i className='fas fa-copyright' /> {`${copyrightDate}`}
+        </span>
 
-        {/* 备案信息保留（如果不需要也可以注释掉） */}
-       {/* 
-         {siteConfig('BEI_AN') && (
+        {/* 2. 博客标题 (ZBblog) */}
+        <span className='font-medium'>
+          {title}
+        </span>
+
+        {/* 3. Powered by 信息 */}
+        <span className='text-xs font-serif text-gray-500 dark:text-gray-300'>
+          Powered by{' '}
+          <a
+            href='https://github.com/tangly1024/NotionNext'
+            className='underline hover:text-blue-500 transition-colors'>
+            NotionNext {siteConfig('VERSION')}
+          </a>
+        </span>
+      </div>
+
+      {/* 下面是备案信息，如果你的 Notion 里没填 BEI_AN，这部分会自动隐藏 */}
+      <div className='mt-1 text-xs'>
+        {siteConfig('BEI_AN') && (
           <>
             <i className='fas fa-shield-alt' />{' '}
             <a href={siteConfig('BEI_AN_LINK')} className='mr-2'>
               {siteConfig('BEI_AN')}
             </a>
-            <br />
           </>
         )}
         <BeiAnGongAn />
-        */}
-        
-        {/* 不蒜子访问统计（原代码是 hidden，保持不动） */}
-      {/*
-        <span className='hidden busuanzi_container_site_pv'>
-          <i className='fas fa-eye' />
-          <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-        </span>
-        <span className='pl-2 hidden busuanzi_container_site_uv'>
-          <i className='fas fa-users' />{' '}
-          <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-        </span>
-        */}
-        {/* 【修改点 3】注释掉 h1 标题（即图片里的 ZBblog）和多余的换行 */}
-        {/* <br />
-        <h1>{title}</h1> */}
+      </div>
 
-        <span className='text-xs font-serif text-gray-500 dark:text-gray-300 '>
-          Powered by{' '}
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className='underline '>
-            NotionNext {siteConfig('VERSION')}
-          </a>
-          .
-        </span>
+      {/* 隐藏的统计组件保持不动 */}
+      <span className='hidden busuanzi_container_site_pv'>
+        <i className='fas fa-eye' />
+        <span className='px-1 busuanzi_value_site_pv'> </span>
+      </span>
+      <span className='pl-2 hidden busuanzi_container_site_uv'>
+        <i className='fas fa-users' />
+        <span className='px-1 busuanzi_value_site_uv'> </span>
       </span>
     </footer>
   )
