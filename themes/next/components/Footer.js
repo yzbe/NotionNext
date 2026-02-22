806f2d6a7861638fc8ce5d70f9e4f02b72b7283c
yzbe <68817477+yzbe@users.noreply.github.com>
2026-02-22 18:19:07 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -10,9 +10,11 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'>
+    // 【修改点 1】将 p-6 改为 py-2，大幅缩小上下内边距，让底部更窄、更靠下
+    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm py-2 bg-white dark:text-gray-400'>
       <span>
-        <DarkModeButton />
+        {/* 【修改点 2】注释掉暗黑模式按钮、版权、心形和作者名这一行 */}
+        {/* <DarkModeButton />
         <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
         <span className='mx-1 animate-pulse'>
           <i className='fas fa-heart' />
@@ -20,8 +22,11 @@ const Footer = ({ title }) => {
         <a href={siteConfig('LINK')} className='underline font-bold '>
           {siteConfig('AUTHOR')}
         </a>
-        .<br />
-        {siteConfig('BEI_AN') && (
+        .<br /> */}
+
+        {/* 备案信息保留（如果不需要也可以注释掉） */}
+       {/* 
+         {siteConfig('BEI_AN') && (
           <>
             <i className='fas fa-shield-alt' />{' '}
             <a href={siteConfig('BEI_AN_LINK')} className='mr-2'>
@@ -31,6 +36,10 @@ const Footer = ({ title }) => {
           </>
         )}
         <BeiAnGongAn />
+        */}
+        
+        {/* 不蒜子访问统计（原代码是 hidden，保持不动） */}
+      {/*
         <span className='hidden busuanzi_container_site_pv'>
           <i className='fas fa-eye' />
           <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
@@ -39,9 +48,12 @@ const Footer = ({ title }) => {
           <i className='fas fa-users' />{' '}
           <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
         </span>
-        <br />
-        <h1>{title}</h1>
-        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>
+        */}
+        {/* 【修改点 3】注释掉 h1 标题（即图片里的 ZBblog）和多余的换行 */}
+        {/* <br />
+        <h1>{title}</h1> */}
+
+        <span className='text-xs font-serif text-gray-500 dark:text-gray-300 '>
           Powered by{' '}
           <a
             href='https://github.com/tangly1024/NotionNext'
