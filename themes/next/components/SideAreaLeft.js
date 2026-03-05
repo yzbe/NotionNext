e315351dde06d6fc3ec1c5309acbbeb31aca7698
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-05 20:24:51 +0800

    Update SideAreaLeft.js

--- themes/next/components/SideAreaLeft.js
+++ themes/next/components/SideAreaLeft.js
@@ -12,10 +12,7 @@ import Toc from './Toc'
 
 /**
  * 侧边平铺
- * @param tags
- * @param currentTag
- * @param post
- * @param currentSearch
+ * @param props
  * @returns {JSX.Element}
  * @constructor
  */
@@ -23,6 +20,24 @@ const SideAreaLeft = props => {
   const { post, slot, postCount } = props
   const { locale } = useGlobal()
   const showToc = post && post.toc && post.toc.length > 1
+
+  // 提取 InfoCard 及其统计信息的公共部分，避免代码重复
+  const InfoCardGroup = (
+    <div className='bg-white dark:bg-hexo-black-gray duration-200 py-6'>
+      <InfoCard {...props} />
+      <div className='mt-2 text-center dark:text-gray-300 font-light text-xs'>
+        <span className='px-1 '>
+          <strong className='font-medium'>{postCount}</strong>
+          {locale.COMMON.POSTS}
+        </span>
+        <span className='px-1 busuanzi_container_site_uv hidden'>
+          | <strong className='pl-1 busuanzi_value_site_uv font-medium' />
+          {locale.COMMON.VISITORS}
+        </span>
+      </div>
+    </div>
+  )
+
   return (
     <aside
       id='left'
@@ -31,7 +46,7 @@ const SideAreaLeft = props => {
         ' hidden lg:block flex-col w-60 relative z-30'
       }>
       <section className='w-60'>
-        {/* 菜单 */}
+        {/* 顶部菜单区块 */}
         <section className='shadow hidden lg:block mb-5 pb-4 bg-white dark:bg-hexo-black-gray hover:shadow-xl duration-200'>
           <Logo className='min-h-32 ' {...props} />
           <div className='pt-2 px-2 '>
@@ -45,38 +60,30 @@ const SideAreaLeft = props => {
         </section>
       </section>
 
+      {/* 侧边跟随区块 */}
       <div className='sticky top-4 hidden lg:block'>
         <Card>
-          <Tabs>
-            {showToc && (
+          {/* 【关键修改逻辑】
+              如果有目录：使用 Tabs 组件切换显示目录和个人信息
+              如果没有目录（如主页）：直接显示个人信息，不使用 Tabs 包装，防止折叠
+          */}
+          {showToc ? (
+            <Tabs>
               <div
                 key={locale.COMMON.TABLE_OF_CONTENTS}
                 className='dark:text-gray-400 text-gray-600 bg-white dark:bg-hexo-black-gray duration-200'>
                 <Toc toc={post.toc} />
               </div>
-            )}
-
-            <div
-              key={locale.NAV.ABOUT}
-              className='mb-5 bg-white dark:bg-hexo-black-gray duration-200 py-6'>
-              <InfoCard {...props} />
-              <>
-                <div className='mt-2 text-center dark:text-gray-300 font-light text-xs'>
-                  <span className='px-1 '>
-                    <strong className='font-medium'>{postCount}</strong>
-                    {locale.COMMON.POSTS}
-                  </span>
-                  <span className='px-1 busuanzi_container_site_uv hidden'>
-                    |{' '}
-                    <strong className='pl-1 busuanzi_value_site_uv font-medium' />
-                    {locale.COMMON.VISITORS}
-                  </span>
-                  {/* <span className='px-1 busuanzi_container_site_pv hidden'>
-                | <strong className='pl-1 busuanzi_value_site_pv font-medium'/>{locale.COMMON.VIEWS}</span> */}
-                </div>
-              </>
+              <div key={locale.NAV.ABOUT}>
+                {InfoCardGroup}
+              </div>
+            </Tabs>
+          ) : (
+            /* 主页或无目录页面直接显示，确保 100% 展开 */
+            <div className='p-2'>
+                {InfoCardGroup}
             </div>
-          </Tabs>
+          )}
         </Card>
 
         <div className='flex justify-center'>
@@ -87,4 +94,5 @@ const SideAreaLeft = props => {
     </aside>
   )
 }
+
 export default SideAreaLeft
