7850450fafc93121150e2ee0a0d9d6a41f237f94
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 01:40:35 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -1,26 +1,14 @@
-import { siteConfig } from '@/lib/config'
-import SmartLink from '@/components/SmartLink'
-
+// themes/next/components/Logo.js
 const Logo = props => {
   const { className } = props
   return (
     <SmartLink href='/' passHref legacyBehavior>
-      {/* 移除 style，背景设为 bg-transparent，去掉补丁感 */}
-      <div
-        className={
-          'flex flex-col justify-center items-center cursor-pointer bg-transparent ' +
-          className
-        }>
-        <div
-          data-aos='fade-down'
-          data-aos-duration='500'
-          data-aos-once='true'
-          data-aos-anchor-placement='top-bottom'
-          className='font-serif text-xl text-white text-center'>
+      {/* 关键：去掉 bg-black 或 style，改为 bg-transparent */}
+      <div className={'flex flex-col justify-center items-center cursor-pointer bg-transparent ' + className}>
+        <div className='font-serif text-xl text-white text-center bg-transparent'>
           {siteConfig('TITLE')}
         </div>
       </div>
     </SmartLink>
   )
 }
-export default Logo
