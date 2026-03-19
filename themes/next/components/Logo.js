6053c9662db3947cc70e954cff3a59b1e1ca2840
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 01:34:26 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -5,27 +5,19 @@ const Logo = props => {
   const { className } = props
   return (
     <SmartLink href='/' passHref legacyBehavior>
-      {/* 这里的 style 确保背景色强制为 #1F2937 */}
+      {/* 移除 style，背景设为 bg-transparent，去掉补丁感 */}
       <div
-        style={{ backgroundColor: '#1F2937' }}
         className={
-          'flex flex-col justify-center items-center cursor-pointer font-bold w-full h-full min-h-[60px] md:min-h-[120px] ' +
+          'flex flex-col justify-center items-center cursor-pointer bg-transparent ' +
           className
         }>
-        
         <div
           data-aos='fade-down'
           data-aos-duration='500'
           data-aos-once='true'
           data-aos-anchor-placement='top-bottom'
-          className='font-serif text-xl md:text-2xl text-white bg-transparent text-center px-4'>
+          className='font-serif text-xl text-white text-center'>
           {siteConfig('TITLE')}
-        </div>
-        
-        {/* 描述文字 - 可选 */}
-        <div
-          className='text-xs text-gray-400 font-light text-center bg-transparent mt-1 hidden md:block'>
-          {siteConfig('DESCRIPTION')}
         </div>
       </div>
     </SmartLink>
