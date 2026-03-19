188c09444ed0812c6b13834da3309d335e23dba6
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 01:48:07 +0800

    Update TopNav.js

--- themes/next/components/TopNav.js
+++ themes/next/components/TopNav.js
@@ -53,7 +53,7 @@ const TopNav = (props) => {
 
   const [isOpen, changeShow] = useState(false)
 
-  // 监听路由
+  // 监听滚动
   useEffect(() => {
     router.events.on('routeChangeComplete', menuCollapseHide)
     return () => {
@@ -62,7 +62,7 @@ const TopNav = (props) => {
   }, [])
 
   /**
-   * 点击切换页面后关闭折叠菜单
+   * 点击切换页面后关闭这点菜单
    */
   const menuCollapseHide = () => {
     changeShow(false)
@@ -81,9 +81,77 @@ const TopNav = (props) => {
     }
   }
 
-  // 搜索栏抽屉插槽
+  //   搜索栏
   const searchDrawerSlot = <>
         {categories && (
             <section className='mt-8'>
                 <div className='text-sm flex flex-nowrap justify-between font-light px-2'>
-                    <div className='text-gray-600 dark:text-gray-200'><i className='mr-2 fas fa-th-
+                    <div className='text-gray-600 dark:text-gray-200'><i className='mr-2 fas fa-th-list' />{locale.COMMON.CATEGORY}</div>
+                    <SmartLink
+                        href={'/category'}
+                        passHref
+                        className='mb-3 text-gray-500 hover:text-black dark:text-gray-400 dark:hover:text-white hover:underline cursor-pointer'>
+
+                        {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
+
+                    </SmartLink>
+                </div>
+                <CategoryGroup currentCategory={currentCategory} categories={categories} />
+            </section>
+        )}
+
+        {tags && (
+            <section className='mt-4'>
+                <div className='text-sm py-2 px-2 flex flex-nowrap justify-between font-light dark:text-gray-200'>
+                    <div className='text-gray-600 dark:text-gray-200'><i className='mr-2 fas fa-tag' />{locale.COMMON.TAGS}</div>
+                    <SmartLink
+                        href={'/tag'}
+                        passHref
+                        className='text-gray-500 hover:text-black  dark:hover:text-white hover:underline cursor-pointer'>
+
+                        {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
+
+                    </SmartLink>
+                </div>
+                <div className='p-2'>
+                    <TagGroups tags={tags} currentTag={currentTag} />
+                </div>
+            </section>
+        )}
+    </>
+
+  return (
+        <div id='top-nav' className='block lg:hidden'>
+            <SearchDrawer cRef={searchDrawer} slot={searchDrawerSlot} />
+
+            {/* 导航栏 */}
+            <div id='sticky-nav' className={`${siteConfig('NEXT_NAV_TYPE', null, CONFIG) !== 'normal' ? 'fixed' : 'relative'} lg:relative w-full top-0 z-20 transform duration-500`}>
+                <div className='w-full flex justify-between items-center p-4 bg-black dark:bg-gray-800 text-white'>
+                    {/* 左侧LOGO 标题 */}
+                    <div className='flex flex-none flex-grow-0'>
+                        <div onClick={toggleMenuOpen} className='w-8 cursor-pointer'>
+                            {isOpen ? <i className='fas fa-times' /> : <i className='fas fa-bars' />}
+                        </div>
+                    </div>
+
+                    <div className='flex'>
+                        <Logo {...props} />
+                    </div>
+
+                    {/* 右侧功能 */}
+                    <div className='mr-1 flex justify-end items-center text-sm space-x-4 font-serif dark:text-gray-200'>
+                        <div className="cursor-pointer block lg:hidden" onClick={showSearchModal}>
+                            <i className="mr-2 fas fa-search" />
+                        </div>
+                    </div>
+                </div>
+
+                <Collapse collapseRef={collapseRef} type='vertical' isOpen={isOpen}>
+                    <MenuList onHeightChange={(param) => collapseRef.current?.updateCollapseHeight(param)} {...props} from='top' />
+                </Collapse>
+            </div>
+
+        </div>)
+}
+
+export default TopNav
