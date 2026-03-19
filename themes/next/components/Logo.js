3924e48a9c403113dc89d50dedb1c0164d58fe2a
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 01:26:36 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -5,32 +5,26 @@ const Logo = props => {
   const { className } = props
   return (
     <SmartLink href='/' passHref legacyBehavior>
-      {/* 1. 背景改为固定的 #1F2937，移除了 dark 模式下的颜色切换 */}
+      {/* 这里的 style 确保背景色强制为 #1F2937 */}
       <div
         style={{ backgroundColor: '#1F2937' }}
         className={
-          'flex flex-col justify-center items-center cursor-pointer min-h-[120px] w-full ' +
+          'flex flex-col justify-center items-center cursor-pointer font-bold w-full h-full min-h-[60px] md:min-h-[120px] ' +
           className
         }>
         
-        {/* 2. 标题文字：确保背景透明，文字水平居中 */}
         <div
           data-aos='fade-down'
           data-aos-duration='500'
           data-aos-once='true'
           data-aos-anchor-placement='top-bottom'
-          className='font-serif text-2xl text-white bg-transparent text-center'>
+          className='font-serif text-xl md:text-2xl text-white bg-transparent text-center px-4'>
           {siteConfig('TITLE')}
         </div>
-
-        {/* 3. 如果不需要显示描述文字，可以将下面这段 div 删除 */}
+        
+        {/* 描述文字 - 可选 */}
         <div
-          data-aos='fade-down'
-          data-aos-duration='500'
-          data-aos-delay='300'
-          data-aos-once='true'
-          data-aos-anchor-placement='top-bottom'
-          className='text-xs text-gray-400 font-light text-center bg-transparent mt-1'>
+          className='text-xs text-gray-400 font-light text-center bg-transparent mt-1 hidden md:block'>
           {siteConfig('DESCRIPTION')}
         </div>
       </div>
