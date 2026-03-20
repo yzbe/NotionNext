e283af38ff171169ca9fab24cffaeadf69505cb1
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 15:29:47 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -1,8 +1,13 @@
 import { siteConfig } from '@/lib/config'
 import SmartLink from '@/components/SmartLink'
+import LazyImage from '@/components/LazyImage' // 引入 NotionNext 自带的图片懒加载组件
 
 const Logo = props => {
   const { className } = props
+  
+  // 获取配置中的头像地址，默认 fallback 到一个相对路径
+  const authorImage = siteConfig('AUTHOR_IMAGE') || '/avatar.svg' 
+
   return (
     <SmartLink href='/' passHref legacyBehavior>
       <div
@@ -10,27 +15,40 @@ const Logo = props => {
           'flex flex-col justify-center items-center cursor-pointer bg-[#1F2937] dark:bg-[#1F2937] w-full space-y-3 font-bold ' +
           className
         }>
-        <div
-          data-aos='fade-down'
-          data-aos-duration='500'
-          data-aos-once='true'
-          data-aos-anchor-placement='top-bottom'
-          className='font-serif text-xl text-white logo'>
-          {siteConfig('TITLE')}
-        </div>
         
-        {/* 加入了 hidden lg:block */}
-        {siteConfig('DESCRIPTION') && (
-          <div
+        {/* 新增：移动端专属的圆形头像，点击跳转主页（包含在 SmartLink 内部） */}
+        <div className='block lg:hidden'>
+            {/* 使用 h-10 w-10 控制头像大小，rounded-full 切成正圆形 */}
+            <LazyImage 
+              src={authorImage} 
+              className='h-10 w-10 rounded-full object-cover shadow-sm' 
+              alt={siteConfig('AUTHOR')} 
+            />
+        </div>
+
+        {/* 原有的文字 Logo 和描述，现在全部加上 hidden lg:block，只在电脑端显示 */}
+        <div className='hidden lg:block'>
+            <div
             data-aos='fade-down'
             data-aos-duration='500'
-            data-aos-delay='300'
             data-aos-once='true'
             data-aos-anchor-placement='top-bottom'
-            className='hidden lg:block text-sm text-gray-500 dark:text-gray-300 font-light text-center'>
-            {siteConfig('DESCRIPTION')}
-          </div>
-        )}
+            className='font-serif text-xl text-white logo text-center'>
+            {siteConfig('TITLE')}
+            </div>
+            
+            {siteConfig('DESCRIPTION') && (
+            <div
+                data-aos='fade-down'
+                data-aos-duration='500'
+                data-aos-delay='300'
+                data-aos-once='true'
+                data-aos-anchor-placement='top-bottom'
+                className='text-sm text-gray-500 dark:text-gray-300 font-light text-center mt-3'>
+                {siteConfig('DESCRIPTION')}
+            </div>
+            )}
+        </div>
 
       </div>
     </SmartLink>
