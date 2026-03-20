b1e73343a3153cc289d5413ad1d270f6446a027d
yzbe <68817477+yzbe@users.noreply.github.com>
2026-03-20 13:24:55 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -7,8 +7,7 @@ const Logo = props => {
     <SmartLink href='/' passHref legacyBehavior>
       <div
         className={
-          //'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 font-bold ' + //原顶部
-          'flex flex-col justify-center items-center cursor-pointer bg-[#1F2937] dark:bg-[#1F2937] w-full space-y-3 font-bold ' + //新顶部260320
+          'flex flex-col justify-center items-center cursor-pointer bg-[#1F2937] dark:bg-[#1F2937] w-full space-y-3 font-bold ' +
           className
         }>
         <div
@@ -17,19 +16,23 @@ const Logo = props => {
           data-aos-once='true'
           data-aos-anchor-placement='top-bottom'
           className='font-serif text-xl text-white logo'>
-          {' '}
+          {/* 去掉了这里多余的空格符，让居中更绝对 */}
           {siteConfig('TITLE')}
         </div>
-        <div
-          data-aos='fade-down'
-          data-aos-duration='500'
-          data-aos-delay='300'
-          data-aos-once='true'
-          data-aos-anchor-placement='top-bottom'
-          className='text-sm text-gray-300 font-light text-center'>
-          {' '}
-          {siteConfig('DESCRIPTION')}
-        </div>
+        
+        {/* 修改点：加入条件判断，只有当 DESCRIPTION 存在时才渲染这块 div */}
+        {siteConfig('DESCRIPTION') && (
+          <div
+            data-aos='fade-down'
+            data-aos-duration='500'
+            data-aos-delay='300'
+            data-aos-once='true'
+            data-aos-anchor-placement='top-bottom'
+            className='text-sm text-gray-500 dark:text-gray-300 font-light text-center'>
+            {siteConfig('DESCRIPTION')}
+          </div>
+        )}
+
       </div>
     </SmartLink>
   )
