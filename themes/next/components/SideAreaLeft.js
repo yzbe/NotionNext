56d0afe040b0726a34de3bd328cdbfae4ab2eecc
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-01 15:21:29 +0800

    Update SideAreaLeft.js

--- themes/next/components/SideAreaLeft.js
+++ themes/next/components/SideAreaLeft.js
@@ -47,7 +47,11 @@ const SideAreaLeft = props => {
       }>
       <section className='w-60'>
         {/* 顶部菜单区块 */}
-        <section className='shadow hidden lg:block mb-5 pb-4 bg-white dark:bg-hexo-black-gray hover:shadow-xl duration-200'>
+        {/* ⭐️ 核心魔法：在这里加上了强制圆角和防溢出 */}
+        <section 
+          style={{ borderRadius: '16px' }}
+          className='rounded-xl overflow-hidden shadow hidden lg:block mb-5 pb-4 bg-white dark:bg-hexo-black-gray hover:shadow-xl duration-200'
+        >
           <Logo className='min-h-32 ' {...props} />
           <div className='pt-2 px-2 '>
             <MenuList allowCollapse={true} {...props} />
