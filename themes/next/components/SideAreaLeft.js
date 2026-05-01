d895b4ddfd59128aa86624f4de560556eb5fdc84
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-01 19:39:20 +0800

    Update SideAreaLeft.js

--- themes/next/components/SideAreaLeft.js
+++ themes/next/components/SideAreaLeft.js
@@ -66,7 +66,7 @@ const SideAreaLeft = props => {
 
       {/* 侧边跟随区块 */}
       {/*  top-*，滑动悬浮时的顶部留白 */}
-      <div className='sticky top-0 hidden lg:block'>
+      <div className='sticky top-8 hidden lg:block'>
         <Card>
           {/* 【关键修改逻辑】
               如果有目录：使用 Tabs 组件切换显示目录和个人信息
