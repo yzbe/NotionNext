48c937dc03327b52988bd06554ad5f39c70ab184
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-01 11:18:46 +0800

    Update Card.js

--- themes/next/components/Card.js
+++ themes/next/components/Card.js
@@ -5,11 +5,14 @@
  */
 const Card = (props) => {
   const { children, headerSlot, className } = props
-  return <div className={className}>
-        <>{headerSlot}</>
-        <section className="shadow px-2 py-4 bg-white dark:bg-hexo-black-gray hover:shadow-xl duration-200">
-            {children}
-        </section>
+  return (
+    <div className={className}>
+      <>{headerSlot}</>
+      {/* ⭐️ 核心修改点：在这里的 section 加入 rounded-xl 和 overflow-hidden */}
+      <section className="rounded-xl overflow-hidden shadow px-2 py-4 bg-white dark:bg-hexo-black-gray hover:shadow-xl duration-200">
+        {children}
+      </section>
     </div>
+  )
 }
 export default Card
