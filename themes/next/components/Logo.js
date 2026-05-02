0574b2908a3c7c78cfd6e2bdaaeee1187c51ea39
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-03 02:20:40 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -11,8 +11,9 @@ const Logo = props => {
     <SmartLink href='/' passHref legacyBehavior>
       <div
         className={
-          // ⭐️ 清理完毕：去除了所有的 shadow 和 hover 特效，保留渐变背景和圆角排版
-          'flex flex-col justify-center items-center cursor-pointer bg-transparent lg:bg-gradient-to-br lg:from-[#eef0f1] lg:to-[#9ca3af] lg:border lg:border-gray-200 dark:border-transparent lg:rounded-xl lg:py-5 lg:mb-4 lg:mx-1 lg:mt-1 lg:w-auto w-full space-y-3 font-bold ' +
+          // ⭐️ 核心修改点：
+          // 删除了这里的 lg:mb-4，彻底消除 Logo 下方多余的空隙！
+          'flex flex-col justify-center items-center cursor-pointer bg-transparent lg:bg-gradient-to-br lg:from-[#eef0f1] lg:to-[#9ca3af] lg:border lg:border-gray-200 dark:border-transparent lg:rounded-xl lg:py-5 lg:mx-1 lg:mt-1 lg:w-auto w-full space-y-3 font-bold ' +
           className
         }>
             
