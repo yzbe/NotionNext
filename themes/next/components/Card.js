206c6a7c9f00e7ae84dceb9a4fc78a67869f753d
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-03 00:02:04 +0800

    Update Card.js

--- themes/next/components/Card.js
+++ themes/next/components/Card.js
@@ -13,7 +13,7 @@ const Card = (props) => {
           这个参数的意思是：无偏移量(0 0)，扩散半径20px，使用6%透明度的纯白光。
           这会让卡片四周均匀发光，完美模拟白天悬浮时四周的微弱漫反射效果！
       */}
-      <section className="rounded-xl overflow-hidden shadow px-2 py-4 bg-white dark:bg-hexo-black-gray duration-300 hover:shadow-xl dark:hover:shadow-[0_0_20px_rgba(255,255,255,0.06)]">
+      <section className="rounded-xl overflow-hidden shadow px-2 py-4 bg-white dark:bg-hexo-black-gray duration-300 hover:shadow-xl dark:hover:shadow-[0_0_20px_rgba(255,255,255,0.2)]">
         {children}
       </section>
     </div>
