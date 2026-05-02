ccafba77221585ec7df1674a4d8f2d46cf8343be
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-02 21:39:11 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -14,7 +14,7 @@ const Logo = props => {
       <div
         className={
           // ⭐️ 核心修改：将日间模式桌面端的背景色 lg:bg-white 替换为了 lg:bg-[#eef0f1]
-          'flex flex-col justify-center items-center cursor-pointer bg-transparent lg:bg-[#e0e4e5] dark:lg:bg-hexo-black-gray lg:border lg:border-gray-100 dark:lg:border-gray-700/50 lg:shadow-sm lg:hover:shadow-md lg:rounded-xl lg:py-5 lg:mb-4 lg:mx-3 lg:mt-3 lg:w-auto transform transition-all duration-300 lg:hover:-translate-y-1 dark:lg:hover:bg-gray-800 w-full space-y-3 font-bold ' +
+          'flex flex-col justify-center items-center cursor-pointer bg-transparent lg:bg-[#e8eff7] dark:lg:bg-hexo-black-gray lg:border lg:border-gray-100 dark:lg:border-gray-700/50 lg:shadow-sm lg:hover:shadow-md lg:rounded-xl lg:py-5 lg:mb-4 lg:mx-3 lg:mt-3 lg:w-auto transform transition-all duration-300 lg:hover:-translate-y-1 dark:lg:hover:bg-gray-800 w-full space-y-3 font-bold ' +
           className
         }>
             
