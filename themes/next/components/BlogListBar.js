7bb0381c9bb0fb300cd9965424e50233da17cf28
yzbe <68817477+yzbe@users.noreply.github.com>
2026-05-03 17:57:38 +0800

    Update BlogListBar.js

--- themes/next/components/BlogListBar.js
+++ themes/next/components/BlogListBar.js
@@ -1,4 +1,3 @@
-
 import CategoryList from './CategoryList'
 import StickyBar from './StickyBar'
 import TagList from './TagList'
@@ -11,17 +10,30 @@ import TagList from './TagList'
 export default function BlogListBar(props) {
   const { tagOptions, tag } = props
   const { category, categoryOptions } = props
+  
   if (tag) {
     return (
-            <StickyBar>
-                <TagList tagOptions={tagOptions} currentTag={tag} />
-            </StickyBar>
+      <StickyBar>
+        {/* ⭐️ 注入统一圆角、背景色和悬浮白光特效 */}
+        <div 
+          style={{ borderRadius: '12px' }}
+          className='p-4 mb-4 bg-white dark:bg-hexo-black-gray shadow transition-shadow duration-300 md:hover:shadow-2xl dark:md:hover:shadow-[0_20px_50px_-5px_#ffffff26,0_8px_20px_-6px_#ffffff1a]'
+        >
+          <TagList tagOptions={tagOptions} currentTag={tag} />
+        </div>
+      </StickyBar>
     )
   } else if (category) {
     return (
-            <StickyBar>
-                <CategoryList currentCategory={category} categoryOptions={categoryOptions} />
-            </StickyBar>
+      <StickyBar>
+        {/* ⭐️ 注入统一圆角、背景色和悬浮白光特效 */}
+        <div 
+          style={{ borderRadius: '12px' }}
+          className='p-4 mb-4 bg-white dark:bg-hexo-black-gray shadow transition-shadow duration-300 md:hover:shadow-2xl dark:md:hover:shadow-[0_20px_50px_-5px_#ffffff26,0_8px_20px_-6px_#ffffff1a]'
+        >
+          <CategoryList currentCategory={category} categoryOptions={categoryOptions} />
+        </div>
+      </StickyBar>
     )
   } else {
     return <></>
